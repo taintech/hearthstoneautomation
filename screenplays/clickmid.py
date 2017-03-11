3d30c41b77bc9f473089ba998d7af878100040b4
taintech <rinattainov@gmail.com>
2017-03-11 23:11:12 +0100

    changed timings

--- screenplays/clickmid.py
+++ screenplays/clickmid.py
@@ -2,9 +2,9 @@ import commonhelpers.quartzcoregraphics as ui
 import time
 
 while True:
+    time.sleep(10)
     ui.mouseclick(100,100)
-    time.sleep(5)
+    time.sleep(300)
     ui.mouseclick(100,200)
-    time.sleep(30)
 
 
